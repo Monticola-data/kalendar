0b844c7899cb348dd1b336ebda209757e4c4c26e
Monticola-data <bursova@monticola-data.cz>
2025-03-09 10:58:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -108,22 +108,24 @@ function renderCalendar() {
             modal.style.display = "block";
             },
 
-            eventContent: function(arg) {
-                let icon = "";
-                let title = arg.event.title;
+eventContent: function(arg) {
+    let icon = "";
+    let title = arg.event.title;
 
-                if (arg.event.extendedProps.predane) {
-                    icon = "✍️"; // Předané
-                    title = title.toUpperCase();
-                } else if (arg.event.extendedProps.hotove) {
-                    icon = "✅"; // Hotové
-                    title = title.toUpperCase();
-                } else if (arg.event.extendedProps.odeslane) {
-                    icon = "📩"; // Odeslané
-                    title = title.toUpperCase();
-                }
-                return { html: `<b>${icon}</b> ${title}` };
-            }
+    if (arg.event.extendedProps.predane) {
+        icon = "✍️"; // Předané
+        title = title.toUpperCase();
+    } else if (arg.event.extendedProps.hotove) {
+        icon = "✅"; // Hotové
+        title = title.toUpperCase();
+    } else if (arg.event.extendedProps.odeslane) { // 🟢 Přidáno
+        icon = "📩"; // Odeslané
+        title = title.toUpperCase();
+    }
+
+    return { html: `<b>${icon}</b> ${title}` };
+}
+
         });       
         calendar.render();
     }
