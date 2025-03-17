9eac1757b14e6138338a6b3ed6a4ce7451dedf39
Monticola-data <bursova@monticola-data.cz>
2025-03-17 12:50:10 +0100

    Create holidays.js

---
+++ holidays.js
@@ -0,0 +1,16 @@
+export const holidays = [
+    { title: "Nový rok", date: "2025-01-01" },
+    { title: "Velký pátek", date: "2025-04-18" },
+    { title: "Velikonoční pondělí", date: "2025-04-21" },
+    { title: "Svátek práce", date: "2025-05-01" },
+    { title: "Den vítězství", date: "2025-05-08" },
+    { title: "Den slovanských věrozvěstů", date: "2025-07-05" },
+    { title: "Den upálení mistra Jana Husa", date: "2025-07-06" },
+    { title: "Den české státnosti", date: "2025-09-28" },
+    { title: "Den české státnosti", date: "2025-09-28" },
+    { title: "Den vzniku samostatného Československa", date: "2025-10-28" },
+    { title: "Den boje za svobodu a demokracii", date: "2025-11-17" },
+    { title: "Štědrý den", date: "2025-12-24" },
+    { title: "1. svátek vánoční", date: "2025-12-25" },
+    { title: "2. svátek vánoční", date: "2025-12-26" }
+];
