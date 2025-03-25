d58b7db9a46b5184b6afd3bdc44a07b35681f60b
Monticola-data <bursova@monticola-data.cz>
2025-03-25 17:24:03 +0100

    Create state.js

---
+++ state.js
@@ -0,0 +1,14 @@
+export const state = {
+    calendar: null,
+    calendarEl: null,
+    modal: null,
+    modalOverlay: null,
+    partySelect: null,
+    partyFilter: null,
+    strediskoFilter: null,
+
+    allEvents: [],
+    omluvenkyEvents: [],
+    partyMap: {},
+    selectedEvent: null,
+};
